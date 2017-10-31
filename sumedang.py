e081a856eab54a63a7270a92afa2ed2481b7fa63
fajrimualim17 <fajrimualim8@gmail.com>
2017-10-31 23:54:26 +0700

    Update sumedang.py

--- sumedang.py
+++ sumedang.py
@@ -28,14 +28,23 @@ folium.Marker([-6.835437, 107.932226], popup='<i>AUDI PRATAMA DANO REGENCY</i>')
 folium.Marker([-6.834199, 107.929305], popup='<i>PT. Melon Pijar Gas</i>').add_to(maps)
 
 folium.Marker([-6.8451028, 107.9249476], popup='<i>BANK SUMEDANG</i>').add_to(maps)
+
 folium.Marker([-6.842935, 107.9246427], popup='<i>Ngopi Heula</i>').add_to(maps)
+
 folium.Marker([-6.8448237, 107.9245214], popup='<i>Warung Echo</i>').add_to(maps)
+
 folium.Marker([-6.8448036, 107.9242944], popup='<i>Mie Rasa</i>').add_to(maps)
+
 folium.Marker([-6.8453361, 107.9247266], popup='<i>Mebel Monalisa</i>').add_to(maps)
+
 folium.Marker([-6.8449819, 107.9243184], popup='<i>PGAK</i>').add_to(maps)
+
 folium.Marker([-6.8446918, 107.9247126], popup='<i>Rejeki Motor</i>').add_to(maps)
+
 folium.Marker([-6.844681, 107.9246972], popup='<i>Ethree Cell</i>').add_to(maps)
+
 folium.Marker([-6.845101, 107.9243717], popup='<i>Toko Sahabat</i>').add_to(maps)
+
 folium.Marker([-6.8451229, 107.9243798], popup='<i>Toko Panjang Putra</i>').add_to(maps)
 
 
